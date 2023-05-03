94100d86764c0930813bac22e1b7623bb3d4b7fa
Chad C Williams <ccwillia@uvic.ca>
2023-05-03 12:37:48 -0700

    Fixed Data folder

--- Data/gansEEG_ExtractSampleSizeData.py
+++ Data/gansEEG_ExtractSampleSizeData.py
@@ -7,8 +7,8 @@ np.random.seed(1251)
 rnd.seed(1251)
 
 #Load EEG data
-EEGData = np.genfromtxt('data/ganTrialERP_len100.csv', delimiter=',', skip_header=1)
-EEGDataHeader = np.genfromtxt('data/ganTrialERP_len100.csv', delimiter=',', names=True).dtype.names
+EEGData = np.genfromtxt('Full Datasets/ganTrialERP_len100.csv', delimiter=',', skip_header=1)
+EEGDataHeader = np.genfromtxt('Full Datasets/ganTrialERP_len100.csv', delimiter=',', names=True).dtype.names
 
 #Create test file of 400 participants
 testParticipants = np.sort(rnd.sample(range(1,int(np.unique(EEGData[:,0])[-1])+1), 400))
@@ -21,12 +21,12 @@ for participant in testParticipants:
     EEGData = np.delete(EEGData, np.where(EEGData[:,0]==participant), axis=0)
 
 #Save File
-saveFilename = 'data/training_data/Runs/gansTrialERP_len100_TestSS400.csv'
+saveFilename = 'Validation and Test Datasets/gansTrialERP_len100_TestValidationData.csv'
 np.savetxt(saveFilename, testEEGData, delimiter=",", fmt='%f', header=','.join(EEGDataHeader), comments='')
     
 #Iterate through different sample sizes to save data
-for sampleSize in np.arange(5,101,5):
-    for run in range(20):
+for sampleSize in [5, 10, 15, 20, 30, 60, 100]:
+    for run in range(5):
 
         #Determine samples of current sample size
         sampleParticipants = np.sort(rnd.sample(list(np.unique(EEGData[:,0])), sampleSize))
@@ -37,5 +37,5 @@ for sampleSize in np.arange(5,101,5):
             reducedData.extend(EEGData[EEGData[:,0]==participant,:])
 
         #Save file
-        saveFilename = 'data/training_data/Runs/gansTrialERP_len100_SS' + str(sampleSize).zfill(3) + '_Run'+ str(run).zfill(2) + '.csv'
+        saveFilename = 'Training Datasets/gansTrialERP_len100_SS' + str(sampleSize).zfill(3) + '_Run'+ str(run).zfill(2) + '.csv'
         np.savetxt(saveFilename, reducedData, delimiter=",", fmt='%f', header=','.join(EEGDataHeader), comments='')
