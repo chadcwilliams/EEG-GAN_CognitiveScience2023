35966b9d8e0d33c66def13f6a7a7a0cb0d4b06cf
Chad C Williams <ccwillia@uvic.ca>
2023-05-04 11:44:42 -0700

    Updated GANs folder

--- GANs/gansTrainingRunsArray.sh
+++ GANs/gansTrainingRunsArray.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-# Request a GPU partition node and access to 1 GPU
+# Request a GPU partition node and access to 8 GPUs
 #SBATCH -p gpu --gres=gpu:8
-
-#SBATCH --constraint=titanv|titanrtx|quadrortx
 
 # Ensures all allocated cores are on the same node
 #SBATCH -N 1
 
-# Request 1 CPU core
-#SBATCH -n 1
+# Request 2 CPU core
+#SBATCH -n 2
+
+# Request 23GB of memort
 #SBATCH --mem 32G
 
-#SBATCH -t 01:00:00
+# Request 24 hours
+#SBATCH -t 24:00:00
 # Specifiy the output file 
 
-#SBATCH --array=1-20
+# Determine number of files to process
+#SBATCH --array=1-35
 
 # Job handling
 #SBATCH -J arrayjob-readline
@@ -23,8 +25,9 @@
 
 #Load Python etc.
 module load python/3.9.0
-module load gcc/5.4
-module load cuda/8.0.61
+module load gcc/10.2
+module load cuda/11.7.1
+module load cudnn/8.2.0
 
 #Environment
 source ./venv/bin/activate
